53c1f4727875b6910abb154762d5aa3079c29e3b
Darth Linuxer <chaves.camilo@gmail.com>
2023-05-09 16:20:06 -0300

    Modified Front

--- AngularFront/src/app/dynamictable/dynamictable.component.ts
+++ AngularFront/src/app/dynamictable/dynamictable.component.ts
@@ -2,7 +2,7 @@ import { AfterViewInit, Component, OnInit } from '@angular/core';
 import { SignalRService } from '../services/signalr.service';
 import { User } from '../models/user';
 import { HttpService } from '../services/http.service';
-import { Observable } from 'rxjs';
+import { Observable, concat, map, scan } from 'rxjs';
 
 @Component({
   selector: 'app-dynamictable',
@@ -16,16 +16,33 @@ export class DynamictableComponent implements OnInit, AfterViewInit {
   constructor(
     private signalR: SignalRService,
     private http: HttpService) {
-  
+
   }
 
-  ngOnInit(){
-    this.signalR.userHubConn.on("Users", c=>
-    this.data = c);
+  ngOnInit() {
+    this.signalR.userHubConn.on("UserAdded", (newUser:User) => {
+      this.data = this.data!.pipe(
+        map((users: User[]) => [...users, newUser])
+      )});
+      this.signalR.userHubConn.on("UserDeleted", (deletedUser:User) => {
+        this.data = this.data!.pipe(
+          map((users: User[]) => users.filter(user => user.id !== deletedUser.id))
+        )});
+        this.signalR.userHubConn.on("UserUpdated", (updatedUser:User) => {
+          this.data = this.data!.pipe(
+            map((users: User[]) => users.map(user => {
+              if (user.id === updatedUser.id) {
+                return updatedUser;
+              } else {
+                return user;
+              }
+            }))
+          );
+        });
   }
-  
+
   ngAfterViewInit(): void {
-    this.signalR.userHubConn.start().then(() => {      
+    this.signalR.userHubConn.start().then(() => {
       this.fetchData();
     });
   }
@@ -38,7 +55,8 @@ export class DynamictableComponent implements OnInit, AfterViewInit {
   edit(item: any) { }
 
   delete(user: User) {
-    this.signalR.userHubConn.invoke("DeleteUser",user.id);
-   }
+    console.log("Delete button pressed!");
+    this.http.delete("user",user.id.toString())
+  }
 
 }
--- AngularFront/src/app/services/http.service.ts
+++ AngularFront/src/app/services/http.service.ts
@@ -1,36 +1,70 @@
 import { Injectable } from '@angular/core';
 import { Observable, catchError, map, throwError, pipe } from 'rxjs';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpResponse } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HttpService {
-  
+
   private apiUrl = 'http://localhost:5185/api';
-   // The HTTP options for the requests
-   private httpOptions = {
-    headers: new HttpHeaders({
-      'Content-Type': 'application/json'
-    })
-  };
-  
+  // The HTTP options for the requests
+  private headers = new HttpHeaders({
+    'Content-Type': 'application/json'
+  });
+
   constructor(private client: HttpClient) { }
 
-  public get<T>(route:string):Observable<T>
-  {
+  public get<T>(route: string): Observable<T> {
     return this.client.get<T>(`${this.apiUrl}/${route}`).pipe
-    (
-      map(data=>{
-        console.log("Data received:", data);
-        return data
-      }
+      (
+        map(data => {
+          console.log("Data received:", data);
+          return data
+        }
         ),
-      catchError(error => {
-        // handle error
-        return throwError(() => new Error(error));
+        catchError(error => {
+          // handle error
+          return throwError(() => new Error(error));
+        })
+      );
+  }
+
+  public getwithId<T>(route: string, id: string): Observable<T> {
+    return this.client.get<T>(`${this.apiUrl}/${route}/${id}`).pipe
+      (
+        map(data => {
+          console.log("Data received:", data);
+          return data
+        }
+        ),
+        catchError(error => {
+          // handle error
+          return throwError(() => new Error(error));
+        })
+      );
+  }
+
+  public delete(route: string, id: string) {
+    let fullroute = `${this.apiUrl}/${route}/${id}`;
+    console.log("Delete fullroute: ", fullroute);
+    this.client.delete(fullroute, {headers: this.headers});
+  }
+
+  public update<T>(route: string, element: T, id: string) {
+    let fullroute = `${this.apiUrl}/${route}/${id}`;
+    console.log("Delete fullroute: ", fullroute);
+    this.client.put(fullroute, element, { observe: 'response' }).pipe(
+      map<HttpResponse<Object>, HttpResponse<Object>>((response: HttpResponse<Object>) => {
+        if (response.status == 200) {
+          console.log(`route ${route} with Id:${id} updated successful`);
+        }
+        return response;
       })
     );
   }
-}
 
+  public create<T>(route: string, element: T): Observable<Object> {
+    return this.client.post(`${this.apiUrl}/${route}`, element, { headers: this.headers });
+  }
+}
